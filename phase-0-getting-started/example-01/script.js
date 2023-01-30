e25c04ca22399e23ac0bf9f4c9f6563bc87e2a53
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-01-30 23:02:29 +0530

    first-app-using-react

---
+++ phase-0-getting-started/example-01/script.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+    React.createElement(
+      "h1",
+      {
+        style: {
+          textAlign: "center",
+          color: "#FF8008",
+          fontFamily: "Arial",
+        },
+      },
+      "First App Using React"
+    ),
+    document.querySelector("div")
+  );
+  
