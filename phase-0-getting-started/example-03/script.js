5033df78a5fa6e5e6d2334fbb1748737fa4fbd47
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-02-01 20:48:04 +0530

    nested-elements

---
+++ phase-0-getting-started/example-03/script.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  React.createElement(function () {
+    return React.createElement("div", null, [
+      React.createElement("h1", null, "What is React ?"),
+      React.createElement("p", null, "React is component based library"),
+    ]);
+  }),
+  document.querySelector("div")
+);
