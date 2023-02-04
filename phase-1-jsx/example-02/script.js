05f8c20bb69510c6c8d06cadd059802fcc9662c5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-02-04 21:35:14 +0530

    component-ready

---
+++ phase-1-jsx/example-02/script.js
@@ -0,0 +1,10 @@
+function MyApp() {
+  return (
+    <div>
+      <h1>What is React</h1>
+      <p>React is component based library.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.querySelector("div"));
