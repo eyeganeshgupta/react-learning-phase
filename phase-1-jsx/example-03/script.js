cf477682f3e704bdf29c044e6e273451ea427944
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-02-04 21:39:23 +0530

    MyApp-component-added

---
+++ phase-1-jsx/example-03/script.js
@@ -0,0 +1,15 @@
+function MyApp() {
+  return (
+    <div>
+      <h1>SuperHeroes</h1>
+      <ul>
+        <li>Thor</li>
+        <li>Spider-Man</li>
+        <li>Dr. Strange</li>
+        <li>Captain America</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.querySelector("div"));
