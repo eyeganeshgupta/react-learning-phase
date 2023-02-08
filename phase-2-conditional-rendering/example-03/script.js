58428fb3e64a6933b4e81d119c0298bc77d1002c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-02-08 23:35:05 +0530

    component-added

---
+++ phase-2-conditional-rendering/example-03/script.js
@@ -0,0 +1,17 @@
+function generateRandomValue() {
+  let numArr = [89, 83, 97, 17, 52, 6, 14, 11, 108, 2];
+  let index = parseInt(Math.random() * numArr.length);
+  return numArr[index];
+}
+
+function CheckEvenOdd() {
+  let randomValue = generateRandomValue();
+  return (
+    <section>
+      <h3>Generated Number is {randomValue},</h3>
+      {randomValue % 2 === 0 ? <p>It is an Even.</p> : <p></p>}
+    </section>
+  );
+}
+
+ReactDOM.render(<CheckEvenOdd />, document.querySelector("div"));
