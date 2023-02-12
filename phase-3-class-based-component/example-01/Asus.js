b843a2c2ca630cff42aee1ad5cce89d8e8c56036
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-02-12 23:53:19 +0530

    Asus-component-ready

---
+++ phase-3-class-based-component/example-01/Asus.js
@@ -0,0 +1,15 @@
+class Asus extends React.Component {
+  render() {
+    return (
+      <p>
+        ASUSTek Computer Inc. is a Taiwanese multinational computer and phone
+        hardware and electronics company headquartered in Beitou District,
+        Taipei, Taiwan. Its products include desktop computers, laptops,
+        netbooks, mobile phones, networking equipment, monitors, wi-fi routers,
+        projectors, motherboards, graphics cards, optical storage, multimedia
+        products, peripherals, wearables, servers, workstations, and tablet PCs.
+        The company is also an original equipment manufacturer (OEM).
+      </p>
+    );
+  }
+}
