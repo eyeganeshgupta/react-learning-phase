1fd2c8aab2c5e1dd2705d96a7a66f6a4b711de8c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-02 23:29:44 +0530

    App-component-ready

---
+++ phase-7-event-handling/example-01/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import MyButton from "./MyButton";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyButton />
+      </div>
+    );
+  }
+}
+
+export default App;
