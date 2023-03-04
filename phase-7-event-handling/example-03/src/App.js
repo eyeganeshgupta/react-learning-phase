a7d4e5261814898cec3cdc0c65bfd2eb73982cb4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-04 21:35:48 +0530

    App-component-ready

---
+++ phase-7-event-handling/example-03/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Increment from "./Increment";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Increment />
+      </div>
+    );
+  }
+}
+
+export default App;
