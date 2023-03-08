ba2ab6e0e1afb2d6e63b6fe8a822f97b2f4975f4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-08 23:06:07 +0530

    App-component-ready

---
+++ phase-7-event-handling/example-07/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import MyClicker from "./MyClicker";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyClicker />
+      </div>
+    );
+  }
+}
+
+export default App;
