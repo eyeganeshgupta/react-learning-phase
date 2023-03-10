c6483f055d458178a10659957df2d9230b263f86
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-10 21:01:07 +0530

    App-component-ready

---
+++ phase-7-event-handling/example-09/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import KeyDetector from "./KeyDetector";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <KeyDetector />
+      </div>
+    );
+  }
+}
+
+export default App;
