61999f0570bc8edbdeca8ba8809852dff85cbfb4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-11 23:53:34 +0530

    App-component-ready

---
+++ phase-7-event-handling/example-10/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Task from "./Task";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Task />
+      </div>
+    );
+  }
+}
+
+export default App;
