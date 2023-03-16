3655d18fc1fb860c42ec4032275803cd53aecd70
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-16 18:56:40 +0530

    App-component

---
+++ phase-8-working-with-forms/example-01/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import LoginForm from "./LoginForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default App;
