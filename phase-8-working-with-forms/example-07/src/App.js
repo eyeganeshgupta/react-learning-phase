7809d3ecaf7dfb5becb90a9f0076fe279aba8a44
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-25 23:09:43 +0530

    App-component

---
+++ phase-8-working-with-forms/example-07/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import MyComponent from "./MyComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
