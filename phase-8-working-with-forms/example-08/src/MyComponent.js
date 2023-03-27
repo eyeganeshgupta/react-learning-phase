2a57ee8aeaef1ec84227b0e8f1f6fe6bfe48da1d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-27 18:55:22 +0530

    textArea-controlled-component

--- phase-8-working-with-forms/example-08/src/MyComponent.js
+++ phase-8-working-with-forms/example-08/src/MyComponent.js
@@ -8,6 +8,29 @@ class MyComponent extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(synObj) {
+    this.setState({ textAreaValue: synObj.target.value });
+  }
+
+  handleSubmit(synObj) {
+    synObj.preventDefault();
+    console.log(this.state.textAreaValue);
+    this.setState({ textAreaValue: "" });
+  }
+
+  render() {
+    return (
+      <form className="MyComponent" onSubmit={this.handleSubmit.bind(this)}>
+        <label htmlFor="">Review us</label>
+        <textarea
+          value={this.state.textAreaValue}
+          onChange={this.handleChange}
+        ></textarea>
+        <button>Send</button>
+      </form>
+    );
+  }
 }
 
 export default MyComponent;
