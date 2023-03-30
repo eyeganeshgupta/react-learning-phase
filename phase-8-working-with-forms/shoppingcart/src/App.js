fb07bd3080658ba7004cbcf6ecdbcb88a2fd424d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-03-30 18:27:03 +0530

    App-component

---
+++ phase-8-working-with-forms/shoppingcart/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import ProductList from "./ProductList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <ProductList/>
+      </div>
+    );
+  }
+}
+
+export default App;
