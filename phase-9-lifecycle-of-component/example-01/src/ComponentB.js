a52896e5dda1c821027565c511b0f09a4d500fd4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-04-29 09:25:58 +0530

    ComponentB

---
+++ phase-9-lifecycle-of-component/example-01/src/ComponentB.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+export default class ComponentB extends Component {
+    componentDidMount() {
+        console.log("Component B: componentDidMount()");
+    }
+
+    constructor(props) {
+        super(props);
+        console.log("Component B: constructor()");
+        this.state = {
+            lastName: "Gupta",
+        };
+    }
+
+    static getDerivedStateFromProps() {
+        console.log("Component B: getDerivedStateFromProps()");
+        return null;
+    }
+
+    render() {
+        console.log("Component B: render()");
+
+        return (
+            <div>
+                <h1>{this.state.lastName}</h1>
+            </div>
+        );
+    }
+}
