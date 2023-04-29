f0d07575a05ed32a00d50cc308aa9ce6ae480538
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-04-29 09:24:02 +0530

    App-component

---
+++ phase-9-lifecycle-of-component/example-01/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import ComponentA from "./ComponentA";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ComponentA />
+      </div>
+    );
+  }
+}
+
+export default App;
