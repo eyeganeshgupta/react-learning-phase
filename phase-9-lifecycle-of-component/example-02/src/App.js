2852b20e8edbd3f044c3dffcb0ed996e98ff06ec
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-04-30 10:39:19 +0530

    App-component

---
+++ phase-9-lifecycle-of-component/example-02/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Counter from "./Counter";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Counter />
+            </div>
+        );
+    }
+}
+
+export default App;
